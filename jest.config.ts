1c9de79d9e03c45c6567e60be183420dea996b59
Kyle Pfromer <kpfromer2@gmail.com>
2021-11-24 16:28:29 -0700

    test: fix ts-jest for esm

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,16 @@
-module.exports = {
+export default {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+
+  globals: {
+    extensionsToTreatAsEsm: ['.ts', '.js'],
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+
+  preset: 'ts-jest/presets/js-with-ts-esm',
 };
